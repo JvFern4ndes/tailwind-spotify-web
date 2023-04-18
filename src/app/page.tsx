c54287c2813b67d179ccab4363b7794f279ea407
João Victor <jvfernandes.contato@gmail.com>
2023-04-18 15:35:04 -0300

    feat: creating the header of sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+import { Home as HomeIcon, Search, Library } from "lucide-react"
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav>
-            <a href="">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full" />
+            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
+            <div className="w-3 h-3 bg-green-500 rounded-full" />
+          </div>
+
+          <nav className="space-y-5 mt-10">
+            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+              <HomeIcon />
               Home
             </a>
-            <a href="">
+            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+              <Search />
               Search
             </a>
-            <a href="">
+            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+              <Library />
               Your Library
             </a>
           </nav>
